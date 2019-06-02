82006fdbce4b19179f805b0b54ab3db90dee1a02
Haar-you <zemlya.pole@gmail.com>
2019-06-03 00:22:19 +0900

    入力ボタンを減らした簡易版を制作

---
+++ simple/script.js
@@ -0,0 +1,411 @@
+const compose_choseong = {
+    "ᄀ": "ᄀ",
+    "ᄀᄀ": "ᄁ",
+    "ᄏ": "ᄏ",
+    "ᄀᄃ": "ᅚ",
+    "ᄂ": "ᄂ",
+    "ᄂᄀ": "ᄓ",
+    "ᄂᄂ": "ᄔ",
+    "ᄂᄃ": "ᄕ",
+    "ᄂᄇ": "ᄖ",
+    "ᄂᄉ": "ᅛ",
+    "ᄂᄌ": "ᅜ",
+    "ᄂᄒ": "ᅝ",
+    "ᄃ": "ᄃ",
+    "ᄃᄃ": "ᄄ",
+    "ᄐ": "ᄐ",
+    "ᄃᄀ": "ᄗ",
+    "ᄃᄅ": "ᅞ",
+    "ᄅ": "ᄅ",
+    "ᄅᄂ": "ᄘ",
+    "ᄅᄅ": "ᄙ",
+    "ᄅᄒ": "ᄚ",
+    "ᄅᄋ": "ᄛ",
+    "ᄆ": "ᄆ",
+    "ᄆᄇ": "ᄜ",
+    "ᄆᄋ": "ᄝ",
+    "ᄇ": "ᄇ",
+    "ᄇᄇ": "ᄈ",
+    "ᄑ": "ᄑ",
+    "ᄇᄀ": "ᄞ",
+    "ᄇᄂ": "ᄟ",
+    "ᄇᄃ": "ᄠ",
+    "ᄇᄉ": "ᄡ",
+    "ᄇᄉᄀ": "ᄢ",
+    "ᄇᄉᄃ": "ᄣ",
+    "ᄇᄉᄇ": "ᄤ",
+    "ᄇᄉᄉ": "ᄥ",
+    "ᄇᄉ": "ᄦ",
+    "ᄇ": "ᄧ",
+    "ᄇ": "ᄨ",
+    "ᄇᄐ": "ᄩ",
+    "ᄇᄑ": "ᄪ",
+    "ᄇᄋ": "ᄫ",
+    "ᄇᄇᄋ": "ᄬ",
+    "ᄑᄇ": "ᅖ",
+    "ᄑᄋ": "ᅗ",
+    "ᄉ": "ᄉ",
+    "ᄉᄉ": "ᄊ",
+    "ᄉᄀ": "ᄭ",
+    "ᄉᄂ": "ᄮ",
+    "ᄉᄃ": "ᄯ",
+    "ᄉᄅ": "ᄰ",
+    "ᄉᄆ": "ᄱ",
+    "ᄉᄇ": "ᄲ",
+    "ᄉᄇᄀ": "ᄳ",
+    "ᄉᄉᄉ": "ᄴ",
+    "ᄉᄋ": "ᄵ",
+    "ᄉᄌ": "ᄶ",
+    "ᄉᄎ": "ᄷ",
+    "ᄉᄏ": "ᄸ",
+    "ᄉᄐ": "ᄹ",
+    "ᄉᄑ": "ᄺ",
+    "ᄉᄒ": "ᄻ",
+    "ᄼ": "ᄼ",
+    "ᄼᄼ": "ᄽ",
+    "ᄾ": "ᄾ",
+    "ᄾᄾ": "ᄿ",
+    "ᅀ": "ᅀ",
+    "ᄋ": "ᄋ",
+    "ᄋᄀ": "ᅁ",
+    "ᄋᄃ": "ᅂ",
+    "ᄋᄆ": "ᅃ",
+    "ᄋᄇ": "ᅄ",
+    "ᄋᄉ": "ᅅ",
+    "ᄋᅀ": "ᅆ",
+    "ᄋᄋ": "ᅇ",
+    "ᄋᄌ": "ᅈ",
+    "ᄋᄎ": "ᅉ",
+    "ᄋᄐ": "ᅊ",
+    "ᄋᄑ": "ᅋ",
+    "ᅌ": "ᅌ",
+    "ᄌ": "ᄌ",
+    "ᄌᄌ": "ᄍ",
+    "ᄎ": "ᄎ",
+    "ᄌᄋ": "ᅍ",
+    "ᄎᄏ": "ᅒ",
+    "ᄎᄒ": "ᅓ",
+    "ᅎ": "ᅎ",
+    "ᅎᅎ": "ᅏ",
+    "ᅐ": "ᅐ",
+    "ᅐᅐ": "ᅑ",
+    "ᅔ": "ᅔ",
+    "ᅕ": "ᅕ",
+    "ᄒ": "ᄒ",
+    "ᄒᄒ": "ᅘ",
+    "ᅙ": "ᅙ",
+};
+
+const compose_jungseong = {
+    "ᅡ": "ᅡ",
+    "ᅢ": "ᅢ",
+    "ᅣ": "ᅣ",
+    "ᅤ": "ᅤ",
+    "ᅥ": "ᅥ",
+    "ᅦ": "ᅦ",
+    "ᅧ": "ᅧ",
+    "ᅨ": "ᅨ",
+    "ᅩ": "ᅩ",
+    "ᅩᅡ": "ᅪ",
+    "ᅩᅢ": "ᅫ",
+    "ᅩᅵ": "ᅬ",
+    "ᅭ": "ᅭ",
+    "ᅮ": "ᅮ",
+    "ᅮᅥ": "ᅯ",
+    "ᅮᅦ": "ᅰ",
+    "ᅮᅵ": "ᅱ",
+    "ᅲ": "ᅲ",
+    "ᅳ": "ᅳ",
+    "ᅳᅵ": "ᅴ",
+    "ᅵ": "ᅵ",
+    "ᅡᅩ": "ᅶ",
+    "ᅡᅮ": "ᅷ",
+    "ᅣᅩ": "ᅸ",
+    "ᅣᅭ": "ᅹ",
+    "ᅥᅩ": "ᅺ",
+    "ᅥᅮ": "ᅻ",
+    "ᅥᅳ": "ᅼ",
+    "ᅧᅩ": "ᅽ",
+    "ᅧᅮ": "ᅾ",
+    "ᅩᅥ": "ᅿ",
+    "ᅩᅦ": "ᆀ",
+    "ᅩᅨ": "ᆁ",
+    "ᅩᅩ": "ᆂ",
+    "ᅩᅮ": "ᆃ",
+    "ᅭᅣ": "ᆄ",
+    "ᅭᅤ": "ᆅ",
+    "ᅭᅧ": "ᆆ",
+    "ᅭᅩ": "ᆇ",
+    "ᅭᅵ": "ᆈ",
+    "ᅮᅡ": "ᆉ",
+    "ᅮᅢ": "ᆊ",
+    "ᅮᅥᅳ": "ᆋ",
+    "ᅮᅨ": "ᆌ",
+    "ᅮᅮ": "ᆍ",
+    "ᅲᅡ": "ᆎ",
+    "ᅲᅥ": "ᆏ",
+    "ᅲᅦ": "ᆐ",
+    "ᅲᅧ": "ᆑ",
+    "ᅲᅨ": "ᆒ",
+    "ᅲᅮ": "ᆓ",
+    "ᅲᅵ": "ᆔ",
+    "ᅳᅮ": "ᆕ",
+    "ᅳᅳ": "ᆖ",
+    "ᅳᅵᅮ": "ᆗ",
+    "ᅵᅡ": "ᆘ",
+    "ᅵᅣ": "ᆙ",
+    "ᅵᅩ": "ᆚ",
+    "ᅵᅮ": "ᆛ",
+    "ᅵᅳ": "ᆜ",
+    "ᅵᆞ": "ᆝ",
+    "ᆞ": "ᆞ",
+    "ᆞᅥ": "ᆟ",
+    "ᆞᅮ": "ᆠ",
+    "ᆞᅵ": "ᆡ",
+    "ᆞᆞ": "ᆢ",
+    "ᅡᅳ": "ᆣ",
+    "ᅣᅮ": "ᆤ",
+    "ᅧᅣ": "ᆥ",
+    "ᅩᅣ": "ᆦ",
+    "ᅩᅤ": "ᆧ",
+};
+
+const compose_jongseong = {
+    "ᆨ": "ᆨ",
+    "ᆨᆨ": "ᆩ",
+    "ᆨᆺ": "ᆪ",
+    "ᆿ": "ᆿ",
+    "ᆨᆯ": "ᇃ",
+    "ᆨᆺᆨ": "ᇄ",
+    "ᆨᆫ": "ᇺ",
+    "ᆨᆸ": "ᇻ",
+    "ᆨᆾ": "ᇼ",
+    "ᆨᆿ": "ᇽ",
+    "ᆨᇂ": "ᇾ",
+    "ᆫ": "ᆫ",
+    "ᆫᆽ": "ᆬ",
+    "ᆫᇂ": "ᆭ",
+    "ᆫᆨ": "ᇅ",
+    "ᆫᆮ": "ᇆ",
+    "ᆫᆺ": "ᇇ",
+    "ᆫᇫ": "ᇈ",
+    "ᆫᇀ": "ᇉ",
+    "ᆫᆫ": "ᇿ",
+    "ᆮ": "ᆮ",
+    "ᇀ": "ᇀ",
+    "ᆮᆨ": "ᇊ",
+    "ᆮᆯ": "ᇋ",
+    "ᆯ": "ᆯ",
+    "ᆯᆨ": "ᆰ",
+    "ᆯᆷ": "ᆱ",
+    "ᆯᆸ": "ᆲ",
+    "ᆯᆺ": "ᆳ",
+    "ᆯᇀ": "ᆴ",
+    "ᆯᇁ": "ᆵ",
+    "ᆯᇂ": "ᆶ",
+    "ᆯᆨᆺ": "ᇌ",
+    "ᆯᆫ": "ᇍ",
+    "ᆯᆮ": "ᇎ",
+    "ᆯᆯ": "ᇐ",
+    "ᆯᆷᆨ": "ᇑ",
+    "ᆯᆷᆺ": "ᇒ",
+    "ᆯᆸᆺ": "ᇓ",
+    "ᆯᆸᇂ": "ᇔ",
+    "ᆯᆸᆼ": "ᇕ",
+    "ᆯᆺᆺ": "ᇖ",
+    "ᆯᇫ": "ᇗ",
+    "ᆯᆿ": "ᇘ",
+    "ᆯᇹ": "ᇙ",
+    "ᆷ": "ᆷ",
+    "ᆷᆨ": "ᇚ",
+    "ᆷᆯ": "ᇛ",
+    "ᆷᆸ": "ᇜ",
+    "ᆷᆺ": "ᇝ",
+    "ᆷᆺᆺ": "ᇞ",
+    "ᆷᇫ": "ᇟ",
+    "ᆷᆾ": "ᇠ",
+    "ᆷᇂ": "ᇡ",
+    "ᆷᆼ": "ᇢ",
+    "ᆸ": "ᆸ",
+    "ᆸᆺ": "ᆹ",
+    "ᇁ": "ᇁ",
+    "ᆸᆯ": "ᇣ",
+    "ᆸᇁ": "ᇤ",
+    "ᆸᇂ": "ᇥ",
+    "ᆸᆼ": "ᇦ",
+    "ᇁᆸ": "ᇳ",
+    "ᇁᆼ": "ᇴ",
+    "ᆺ": "ᆺ",
+    "ᆺᆺ": "ᆻ",
+    "ᆺᆨ": "ᇧ",
+    "ᆺᆮ": "ᇨ",
+    "ᆺᆯ": "ᇩ",
+    "ᆺᆸ": "ᇪ",
+    "ᇫ": "ᇫ",
+    "ᆼ": "ᆼ",
+    "ᇰ": "ᇰ",
+    "ᇰᆨ": "ᇬ", "ᆼᆨ": "ᇬ",
+    "ᇰᆨᆨ": "ᇭ", "ᆼᆨᆨ": "ᇭ",
+    "ᇰᇰ": "ᇮ", "ᆼᆼ": "ᇮ",
+    "ᇰᆿ": "ᇯ", "ᆼᆿ": "ᇯ",
+    "ᇰᆺ": "ᇱ", "ᆼᆺ": "ᇱ",
+    "ᇰᇫ": "ᇲ", "ᆼᇫ": "ᇲ",
+    "ᆽ": "ᆽ",
+    "ᆾ": "ᆾ",
+    "ᇂ": "ᇂ",
+    "ᇂᆫ": "ᇵ",
+    "ᇂᆯ": "ᇶ",
+    "ᇂᆷ": "ᇷ",
+    "ᇂᆸ": "ᇸ",
+    "ᇹ": "ᇹ",
+};
+
+var hangul_composition = "";
+
+function constructHungul(str){
+    const m = str.match(/([\u1100-\u115E]*)([\u1161-\u11A7]*)([\u11A8-\u11FF]*)([\u302E-\u302F]*)/);
+    //console.log(m);
+    const choseong = compose_choseong[m[1]];
+    const jungseong = compose_jungseong[m[2]];
+    const jongseong = compose_jongseong[m[3]];
+    const bangjoem = m[4];
+
+    const ret = (choseong ? choseong : "") + (jungseong ? jungseong : "") + (jongseong ? jongseong : "") + (bangjoem ? bangjoem : "");
+
+    return ret;
+}
+
+function showConstructHangul(){
+    $("#div-construct-hangul").empty();
+    $("#div-construct-hangul").append(
+	$("<div></div>", {text: hangul_composition.replace(/./g, "$&‌")}),
+	$("<div></div>", {text: constructHungul(hangul_composition), "class": "h3"})
+    );
+}
+
+function deleteChar(){
+    hangul_composition = hangul_composition.slice(0,-1);
+    showConstructHangul();
+}
+
+function addConstructedHangul(){
+    const $textarea = $("#textarea-1");
+    const text = $textarea.val();
+    $textarea.val(text + constructHungul(hangul_composition));
+
+    
+    hangul_composition = "";
+    showConstructHangul();
+}
+
+$(window).ready(function(){
+    const $textarea = $("#textarea-1");
+    
+    const $div_choseong = $("#choseong");
+    const $div_jungseong = $("#jungseong");
+    const $div_jongseong = $("#jongseong");
+    const $div_bangjeom = $("#bangjeom");
+
+    // U+1100 ~ U+115E
+    "ᄀᄏᄂᄃᄐᄅᄆᄇᄑᄉᄼᄾᅀᄋᅌᄌᄎᅎᅐᅔᅕᄒᅙ".split("").forEach(c => {
+	const $btn = $("<button></button>",
+		       {
+			   "class": "button-hangul",
+			   text: c,
+			   on: {
+			       click: function(){
+				   hangul_composition += c;
+				   showConstructHangul();
+			       }
+			   }
+		       }
+		      );
+	$div_choseong.append($btn);
+    });
+
+        
+    
+    // U+1161 ~ U+11A7
+    "ᅡᅢᅣᅤᅥᅦᅧᅨᅩᅭᅮᅲᅳᅵᆞ".split("").forEach(c => {
+	const $btn = $("<button></button>",
+		       {
+			   "class": "button-hangul",
+			   text: c,
+			   on: {
+			       click: function(){
+				   hangul_composition+= c;
+				   showConstructHangul();
+			       }
+			   }
+		       }
+		      );
+	$div_jungseong.append($btn);
+    });
+
+    
+
+
+    // U+11A8 ~ U+11FF
+    "ᆨᆫᆮᆯᆷᆸᆺᆼᆽᆾᆿᇀᇁᇂᇫᇰᇹ".split("").forEach(c => {
+	const $btn = $("<button></button>",
+		       {
+			   "class": "button-hangul",
+			   text: c,
+			   on: {
+			       click: function(){
+				   hangul_composition += c;
+				   showConstructHangul();
+			       }
+			   }
+		       }
+		      );
+	$div_jongseong.append($btn);
+    });
+
+    // U+302E, U+302F
+    "〮〯".split("").forEach(c => {
+	const $btn = $("<button></button>",
+		       {
+			   "class": "button-hangul",
+			   text: c,
+			   on: {
+			       click: function(){
+				   hangul_composition += c;
+				   showConstructHangul();
+			       }
+			   }
+		       }
+		      );
+	$div_bangjeom.append($btn);
+    });
+
+    // canvas
+    const canvas = document.getElementById("canvas-1");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = $textarea.width();
+    canvas.height = $textarea.height();
+
+    $textarea.focusout(function(){
+	drawHangul();
+    });
+
+});
+
+function drawHangul(){
+    const $textarea = $("#textarea-1");
+    const canvas = document.getElementById("canvas-1");
+    const ctx = canvas.getContext("2d");
+    
+    const font_size = $textarea.css("font-size");
+    const line_height = $textarea.css("line-height");
+    
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillStyle = "black";
+    ctx.font = `${font_size} 'Noto Sans KR'`
+
+    $textarea.val().split("\n").forEach(function(str,index){
+	ctx.fillText(str, 0, parseInt(line_height)*(index+1));
+    });
+}
